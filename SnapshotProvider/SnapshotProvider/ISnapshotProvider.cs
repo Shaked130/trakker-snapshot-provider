68681e9601ea647dc39450dead84c6c65a61bed7
Shaked Shuster <shaked6600@gmail.com>
2020-03-30 04:11:40 +0300

    GetDrives API added

---
+++ SnapshotProvider/SnapshotProvider/ISnapshotProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using TrakkerModels;
+using DriveInfo = TrakkerModels.DriveInfo;
+
+namespace SnapshotProvider
+{
+    internal interface ISnapshotProvider
+    {
+        List<System.IO.DriveInfo> GetDrivesMetadata();
+
+        DriveInfo GetDriveInfo(string driveName);
+
+    }
+}
---
+++ SnapshotProvider/SnapshotProvider/Utilities/Exceptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnapshotProvider.Utilities
+{
+    public static class Exceptions
+    {
+        public class DriveNotFoundException : Exception
+        {
+        }
+
+        public class DriveNotSupportedException : Exception
+        {
+        }
+    }
+}
